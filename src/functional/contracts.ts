bc43e896d18e1bc54dd563d787a1a2173bd78ae8
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-02-12 20:36:10 +0000

    Code cleanup

--- src/functional/contracts.ts
+++ src/functional/contracts.ts
@@ -3,7 +3,6 @@ export type UnaryFunction<T, R> = (p: T, index?: number) => R;
 export type ReducerFunc<T, R> = (previous: R, current: T, index?: number) => R;
 export type FilterFunc<T> = (p: T, index?: number) => boolean;
 export type CollectorFunc<T, R> = (value: Generator<T, void>) => R;
-export type ListCollectorFunc<T> = CollectorFunc<T, T[]>;
 export type ComposeFunc<T, R> = (
   ...funcs: ((...params: any[]) => any)[]
 ) => (source: T) => R;
